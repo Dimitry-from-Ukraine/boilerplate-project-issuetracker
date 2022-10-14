ea110d852b1a60528f153a390a6fde5fef2e82f3
Dimitry-from-Ukraine <dmitry.gubsky@googlemail.com>
2022-10-14 09:46:58 +0100

    updating

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,67 @@
 'use strict';
 
-module.exports = function (app) {
+const removeUnneededIssueFields = ({
+  _id,
+  project_name,
+  issue_title,
+  issue_text,
+  created_by,
+  assigned_to,
+  status_text,
+  open,
+  created_on,
+  updated_on,
+}) => ({
+  _id,
+  project_name,
+  issue_title,
+  issue_text,
+  created_by,
+  assigned_to,
+  status_text,
+  open,
+  created_on,
+  updated_on,
+});
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
-      let project = req.params.project;
-      
+module.exports = async function (app) {
+  // Get issueController middleware (requires waiting for DB connection)
+
+  const {
+    getAllProjectIssues,
+    createNewIssue,
+    updateIssueByID,
+    deleteIssueByID,
+  } = await require('../controllers/issues');
+
+  app
+    .route('/api/issues/:project')
+
+    // GET route to return all issues for a project
+    .get(getAllProjectIssues, (req, res) => {
+      return res.json(
+        res.locals.projectIssues.map((issue) =>
+          removeUnneededIssueFields(issue),
+        ),
+      );
     })
-    
-    .post(function (req, res){
-      let project = req.params.project;
-      
+
+    // POST route to handle creating a new issue for a project
+    .post(createNewIssue, (req, res) => {
+      // Return issue-related document fields:
+      return res.json(removeUnneededIssueFields(res.locals.issueDoc));
     })
-    
-    .put(function (req, res){
-      let project = req.params.project;
-      
+
+    .put(updateIssueByID, (req, res) => {
+      const updateDoc = removeUnneededIssueFields(res.locals.updateDoc);
+      updateDoc.result = 'successfully updated';
+      return res.status(200).json(updateDoc);
     })
-    
-    .delete(function (req, res){
-      let project = req.params.project;
-      
+
+    .delete(deleteIssueByID, (req, res) => {
+      return res.json({
+        result: 'successfully deleted',
+        _id: res.locals.deletedID,
+      });
     });
-    
 };
--- server.js
+++ server.js
@@ -6,55 +6,71 @@ const expect      = require('chai').expect;
 const cors        = require('cors');
 require('dotenv').config();
 
+const DBConnection = require('./db-connect.js');
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
 const runner            = require('./test-runner');
 
-let app = express();
+const app = express();
 
+// Serve static files from /public folder on any request to /public
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(cors({ origin: '*' })); // For FCC testing purposes only
 
-
-
+// Parse request JSON and url encoded bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Sample front-end
-app.route('/:project/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/issue.html');
+// Connect to DB before connecting app routes:
+DBConnection.getClient()
+  .then(async (dbClient) => {
+    // Front end for specific project issues:
+    app.route('/:project/').get(function (req, res) {
+      res.sendFile(__dirname + '/views/issue.html');
+    });
+
+    // Serve index.html page on get request to '/'
+    app.route('/').get(function (req, res) {
+      res.sendFile(__dirname + '/views/index.html');
+    });
+
+    // For FCC testing purposes
+    fccTestingRoutes(app);
+
+    // Routing for API
+    await apiRoutes(app);
+
+    // 404 page not found:
+    app.get('*', (req, res) => {
+      console.log('HIT 404 ROUTE');
+      // Redirect to index
+      res.redirect('/');
+    });
+  })
+  .catch((err) => {
+    // If an error occurs, respond to requests with error message
+    console.error('Error when trying to set up routes with DB: ', err);
+    app.use('*', (req, res) => res.send('Database connection error!'));
   });
 
-//Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-//For FCC testing purposes
-fccTestingRoutes(app);
-
-//Routing for API 
-apiRoutes(app);  
-    
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+// Internal Error Handler:
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Internal Server error: See Server Logs');
 });
 
-//Start our server and tests!
+// Have server listen on PORT or default to 3000
+// http://localhost:3000/
+// If NODE_ENV='test' then tests will be run on startup
 const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
-  if(process.env.NODE_ENV==='test') {
+  if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...');
     setTimeout(function () {
       try {
         runner.run();
-      } catch(e) {
+      } catch (e) {
         console.log('Tests are not valid:');
         console.error(e);
       }
